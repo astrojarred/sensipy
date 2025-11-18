30e21dc4d68e42dd7a4367ee566f80a07e5ef5db
Jarred Green <astrojarred@gmail.com>
2025-11-18 13:55:27 +0100

    Change package name in docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Gravitational Wave Toy',
+			title: 'sensipy',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
